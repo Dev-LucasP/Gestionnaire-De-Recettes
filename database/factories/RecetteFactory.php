dffdcfd94b3a0d092046f047a6bdb46cb0cf0aba
lucas.perez <lucas.perez@info0426.ipa.iutlens.local>
2024-10-18 17:21:08 +0200

    :sparkles: Création d’une classe fabrique RecetteFactory

--- database/factories/RecetteFactory.php
+++ database/factories/RecetteFactory.php
@@ -17,7 +17,13 @@ class RecetteFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'nom' => $this->faker->name(),
+            'description' => $this->faker->sentence(6, true),
+            'categorie' => $this->faker->word(),
+            'visuel' => $this->faker->text(),
+            'temps_preparation' => $this->faker->numberBetween(1, 120),
+            'nb_personnes' => $this->faker->numberBetween(1, 10),
+            'cout' => $this->faker->numberBetween(1, 5),
         ];
     }
 }
