c113eaba3f97626a032f6eb89414a770c5b52496
lucas.perez <lucas.perez@info0317.ipa.iutlens.local>
2024-10-25 12:48:39 +0200

    :sparkles: Ajout des fichiers pour afficher les recettes

---
+++ resources/views/recettes/index.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Liste des recettes</title>
+</head>
+<body>
+<h2>La liste des recettes</h2>
+
+@if(!empty($recettes))
+    <ul>
+        @foreach($recettes as $recette)
+            <li>
+                {{$recette->nom}} {{$recette->description}} {{$recette->categorie}} {{$recette->visuel}} {{$recette->temps_preparation}} {{$recette->nb_personnes}} {{$recette->cout}}
+            </li>
+        @endforeach
+    </ul>
+@else
+    <h3>aucune recette</h3>
+@endif
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\RecetteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/recettes', [RecetteController::class,'index'])->name('recettes.index');
