d4fbf4d155b2d9cc525abb97acdb40b3cdf16141
lucas.perez <lucas.perez@info0116.ipa.iutlens.local>
2024-11-04 10:43:12 +0100

    :sparkles: Add components

---
+++ resources/views/statiques/contact.blade.php
@@ -0,0 +1,19 @@
+
+<!-- resources/views/presentation.blade.php -->
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+    <link rel="stylesheet" href="{{ asset('css/app-layout.css') }}">
+</head>
+<body>
+<x-app-layout>
+    <main>
+        <h1>Mes Contact :</h1>
+        <p>Vous pouvez me contacter par mail à l'adresse suivante : <a href="mailto:lucas.perez59450@gmail.com"></a> </p>
+    </main>
+</x-app-layout>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,15 @@ Route::get('/', function () {
 });
 
 Route::get('/recettes', [RecetteController::class,'index'])->name('recettes.index');
+
+Route::get('/', function () {
+    return view('statiques.accueil', ['titre' => 'Accueil']);
+})->name('accueil');
+
+Route::get('/presentation', function () {
+    return view('statiques.presentation', ['titre' => 'Presentation']);
+})->name('presentation');
+
+Route::get('/contact', function () {
+    return view('statiques.contact', ['titre' => 'Contact']);
+})->name('contact');
