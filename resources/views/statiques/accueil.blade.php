b7c3cf15953f3a1332c40f5aaa0cbc47db285d2f
lucas.perez <lucas.perez@info0116.ipa.iutlens.local>
2024-11-04 11:14:52 +0100

    :sparkles: Add img

--- resources/views/statiques/accueil.blade.php
+++ resources/views/statiques/accueil.blade.php
@@ -1,8 +1,9 @@
 <x-app-layout :titre="$titre">
     <div>
-        <h1>{{$titre}}</h1>
+        <h1>{{ $titre }}</h1>
         <h2>Gestion des tâches</h2>
-
         <p>Bienvenue dans l'application de gestion des tâches.</p>
+        @vite(['resources/css/app.css'])
+        <img src="{{ Vite::asset('resources/images/recette-img.jpg') }}" alt="Example Image" class="centered-image">
     </div>
 </x-app-layout>
